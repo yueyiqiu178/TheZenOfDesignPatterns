611295be09804f121a1ffc4a324d3e680f797bb0
MaxQiu <yueyiqiu178@gmail.com>
2023-11-22 14:49:27 +0800

    begin project

---
+++ src/srp/section3/Client.java
@@ -0,0 +1,37 @@
+package srp.section3;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		new IPhone() {
+
+			@Override
+			public void dial(String phoneNumber) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void call(Object o) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void answer(Object o) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void huangup() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		};
+		
+	}
+
+}
