cca8842e05a1fe725064d8baaa398179e70e6bb0
MaxQiu <yueyiqiu178@gmail.com>
2023-11-23 14:43:30 +0800

    add singleton simple_factory

--- src/ocp/section3/NovelBookTest.java
+++ src/ocp/section3/NovelBookTest.java
@@ -1,13 +1,17 @@
 package ocp.section3;
 
-import junit.framework.TestCase;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * @author cbf4Life cbf4life@126.com
  * I'm glad to share my knowledge with you all.
  * NovelBook的测试类
  */
-public class NovelBookTest extends TestCase {
+public class NovelBookTest{
 	private String name = "平凡的世界";
 	private int price = 6000;
 	private String author = "路遥";
@@ -15,9 +19,10 @@ public class NovelBookTest extends TestCase {
 	private IBook novelBook = new NovelBook(name,price,author);
 
 	//测试getPrice方法
+	@Test
 	public void testGetPrice() {
 		//原价销售，判断输入和输出的值是否相等进行断言
-		super.assertEquals(this.price, this.novelBook.getPrice());
+		assertEquals(this.price, this.novelBook.getPrice());
 	}
 
 }
--- src/ocp/section3/OffNovelBookTest.java
+++ src/ocp/section3/OffNovelBookTest.java
@@ -1,24 +1,28 @@
 package ocp.section3;
 
-import junit.framework.TestCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * @author cbf4Life cbf4life@126.com
  * I'm glad to share my knowledge with you all.
  */
-public class OffNovelBookTest extends TestCase {
+public class OffNovelBookTest{
 	
 	private IBook below40NovelBook = new OffNovelBook("平凡的世界",3000,"路遥");
 	private IBook above40NovelBook = new OffNovelBook("平凡的世界",6000,"路遥");
 	
 	//测试低于40元的数据是否是打8折
+	@Test
 	public void testGetPriceBelow40() {
-		super.assertEquals(2400, this.below40NovelBook.getPrice());
+		assertEquals(2400, this.below40NovelBook.getPrice());
 	}
 	
 	//测试大于40的书籍是否是打9折
+	@Test
 	public void testGetPriceAbove40(){
-		super.assertEquals(5400, this.above40NovelBook.getPrice());
+		assertEquals(5400, this.above40NovelBook.getPrice());
 	}
 
 }
