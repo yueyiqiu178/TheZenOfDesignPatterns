48159ec88c642870f7270cea407918dac51a299a
MaxQiu <yueyiqiu178@gmail.com>
2023-11-29 10:54:49 +0800

    add bridge

--- src/bridge/section2/Client.java
+++ src/bridge/section2/Client.java
@@ -1,4 +1,4 @@
-package bridge.section1;
+package bridge.section2;
 
 /**
  * @author cbf4Life cbf4life@126.com
@@ -9,14 +9,15 @@ public class Client {
 
 	public static void main(String[] args) {
 		System.out.println("-------房地产公司是这个样子运行的-------");
-		// 先找到我的公司
-		HouseCorp houseCorp = new HouseCorp();
-		// 看我怎么挣钱
+		//先找到我的公司
+		HouseCorp houseCorp =new HouseCorp();
+		//看我怎么挣钱
 		houseCorp.makeMoney();
-		System.out.println("\n");
-		System.out.println("-------服装公司是这样运行的-------");
-		ClothesCorp clothesCorp = new ClothesCorp();
-		clothesCorp.makeMoney();
+		System.out.println("\n");	
+		System.out.println("-------山寨公司是这样运行的-------");
+		IPodCorp iPodCorp = new IPodCorp();
+		iPodCorp.makeMoney();
 	}
 }
+
 
